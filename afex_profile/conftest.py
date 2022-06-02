2a6364d993d24a7051242f7465a6db3e82c759df
Tosin Ayodele <tosinayodele@Tosins-MacBook-Pro.local>
2022-06-02 08:38:55 +0100

    initial project setup

---
+++ afex_profile/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from afex_profile.users.models import User
+from afex_profile.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
